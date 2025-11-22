dced964d97291c59657ec9c23b9587e5efc54371
Dylan <viad090723hmclvya2@cbtis217.edu.mx>
2025-11-21 23:16:56 -0600

    Agregados comentarios detallados al codigo

--- EncuentraOcho.cpp
+++ EncuentraOcho.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
-//comentario de prueba
-int main() {
-    int n;
-    cin>>n;
 
-    bool found=false;
-    for (int i=0; i<n; i++) {
+int main() {
+    // Solicitar el número de elementos que se van a leer
+    int n;
+    cin >> n;
+
+    // Variable para contar cuántas veces aparece el número 8
+    int contador = 0;
+
+    // Leer cada número ingresado por el usuario
+    for (int i = 0; i < n; i++) {
         int x;
-        cin>>x;
-        if (x==8) {
-            found=true;
+        cin >> x;
+
+        // Si el número leído es un 8, aumentamos el contador
+        if (x == 8) {
+            contador++;
         }
     }
 
-    cout<<(found?1:0);
+    // Mostrar cuántos números 8 se encontraron
+    cout << contador;
 
+    // Fin del programa
     return 0;
 }
